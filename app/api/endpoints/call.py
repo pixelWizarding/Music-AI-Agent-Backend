9d4f07b62608725a5c75726377d10ebdc00cdce9
pixelWizarding <mukaitsubor@gmail.com>
2024-10-07 11:44:29 +0900

    feat: update prompt function

--- app/api/endpoints/call.py
+++ app/api/endpoints/call.py
@@ -6,9 +6,9 @@ from fastapi.responses import Response
 from twilio.rest import Client as TwilioClient
 from twilio.twiml.voice_response import VoiceResponse, Gather
 from urllib.parse import quote_plus
-from app.services.stream_gpt_text import stream_gpt_text, stream_initial_gpt_response
+from app.services.stream_gpt_text import stream_gpt_text, stream_initial_gpt_response, analyze_response
 from app.db.firestore import get_firestore_db
-from app.schemas.events import Event
+from app.schemas.events import Event, Call
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from typing import Optional
@@ -28,34 +28,59 @@ API_HEADERS = {
     "Authorization": f"Bearer {PLAY_HT_API_KEY}"  # Replace with your PlayHT API key
 }
 
+def get_current_time():
+    return datetime.utcnow() + timedelta(hours=9)
+
 @router.get("/trigger_calls")
 async def trigger_scheduled_calls():
-  db = get_firestore_db()
-  current_time = datetime.utcnow() + timedelta(hours=9)
-  event_docs = db.collection("events").where('is_success', '==', False).where('started_at', '<', current_time).stream()
-  events = [doc.to_dict() for doc in event_docs]
+    db = get_firestore_db()
+    current_time = get_current_time()
+    event_docs = db.collection("events").where('is_success', '==', False).where('started_at', '<', current_time).stream()
+    events = [doc.to_dict() for doc in event_docs]
 
-  for event in events:
-      for company_id in event['company_ids']:
-          contact_docs = db.collection("contacts").where("id", "==", company_id).stream()
+    for event in events:
+        call_records = []
+        event_id = event['id']
+        for company_id in event['company_ids']:
+            contact_docs = db.collection("contacts").where("id", "==", company_id).stream()
 
-          contact_data = None
-          for doc in contact_docs:
-              contact_data = doc.to_dict()
-          
-          if contact_data:
-              to_phone_number = contact_data['phone_number']
-              prompt = event['prompt']
-              await call_prompt(to_phone_number=to_phone_number, voice=event['agent_id'], company=company_id, purpose=prompt, requester='Jin')
+            contact_data = None
+            for doc in contact_docs:
+                contact_data = doc.to_dict()
+            
+            if contact_data:
+                to_phone_number = contact_data['phone_number']
+                prompt = event['prompt']
+                requester='Jin'
+                try:
+                    call_result = await call_prompt(to_phone_number=to_phone_number, voice=event['agent_id'], company=company_id, purpose=prompt, requester=requester, event_id = event_id )
+                    call_record = Call(
+                        call_sid=call_result['call_sid'],
+                        company_id=company_id,
+                        contact_person_name=requester,
+                        started_at=current_time
+                    )
+                
+                except Exception as e:
+                    print(f"Error occurred while calling {to_phone_number} for company {company_id}: {str(e)}")
+                    call_record = Call(
+                        company_id=company_id,
+                        contact_person_name=requester,
+                        status='0',
+                        started_at=current_time                    
+                    )
+                    continue
+                
+                call_records.append(call_record.dict())
 
-          event_update_query = db.collection("events").where('id', '==', event['id']).stream()
-          event_update_docs = [doc for doc in event_update_query]
+        event_update_query = db.collection("events").where('id', '==', event_id).stream()
+        event_update_docs = [doc for doc in event_update_query]
 
-          if event_update_docs:
-              event_ref = event_update_docs[0].reference
-              event_ref.update({'is_success': True, 'updated_at': datetime.utcnow()})
+        if event_update_docs:
+            event_ref = event_update_docs[0].reference
+            event_ref.update({'events': call_records, 'is_success': True, 'updated_at': datetime.utcnow()})
 
-  return {"status": "Calls triggered for events"}
+        return {"status": "Calls triggered for events"}
     
 
 @router.get("/say-prompt")
@@ -67,7 +92,7 @@ async def say_prompt(voice: str, company: str, purpose: str, requester: str, pro
       gpt_stream = stream_gpt_text(prompt, requester, company, purpose, lambda ttfb: headers.update({"X-ChatGPT-TTFB": str(ttfb)}))
   
   if gpt_stream is None:
-            raise ValueError("GPT stream generation failed, no response from OpenAI API.")
+    raise ValueError("GPT stream generation failed, no response from OpenAI API.")
   ssml_text = f"<speak>{''.join([chunk for chunk in gpt_stream])}</speak>"
 
   audio_url = await generate_audio(ssml_text, voice)
@@ -99,45 +124,73 @@ async def generate_audio(ssml_text: str, voice: str) -> str:
 
 
 call_data = defaultdict(lambda: {"count": 0, "receiver_speech": []})
-@router.post("/call-prompt")
-async def call_prompt(to_phone_number: str, voice: str, company: str, purpose: str, requester: str):
-  call = twilio_client.calls.create(
-      to=to_phone_number,
-      from_=TWILIO_PHONE_NUMBER,
-      url=f"https://aef0-78-46-38-10.ngrok-free.app/calls/twilio-stream?voice={quote_plus(voice)}&company={quote_plus(company)}&purpose={quote_plus(purpose)}&requester={quote_plus(requester)}",
-      status_callback=f"https://aef0-78-46-38-10.ngrok-free.app/calls/twilio-status",
-      status_callback_event=['initiated', 'ringing', 'answered', 'completed'],
-      record=True,
-      recording_status_callback=f"https://aef0-78-46-38-10.ngrok-free.app/calls/recording-status",
-  )
-  return {"status": "Call initiated", "call_sid": call.sid}
+async def call_prompt(to_phone_number: str, voice: str, company: str, purpose: str, requester: str, event_id: str):
+    try:
+        call = twilio_client.calls.create(
+            to=to_phone_number,
+            from_=TWILIO_PHONE_NUMBER,
+            url=f"https://beb1-78-46-38-10.ngrok-free.app/calls/twilio-stream?voice={quote_plus(voice)}&company={quote_plus(company)}&purpose={quote_plus(purpose)}&requester={quote_plus(requester)}",
+            record=True,
+            recording_status_callback=f"https://beb1-78-46-38-10.ngrok-free.app/calls/recording-status?event_id={event_id}",
+        )
+        return {"status": "Call initiated", "call_sid": call.sid}
+    except Exception as e:
+        print(f"Error occurred while initiating call: {str(e)}")
+        return {"status": "Call failed", "error": str(e)}
 
-@router.post("/twilio-status")
-async def twilio_status(request: Request):
-    form_data = await request.form()
-
-    call_sid = form_data.get("CallSid")
-    call_status = form_data.get("CallStatus")
-
-    print(f"Call SID: {call_sid}, Status: {call_status}")
-
-    if call_status == "completed":
-        print(f"Call {call_sid} completed")
-    
-    return {"status": "Callback received"}
 
 @router.post("/recording-status")
-async def recording_status(request: Request):
+async def recording_status(event_id: str, request: Request):
     form_data = await request.form()
-
-    # Get the recording details
+    
     call_sid = form_data.get("CallSid")
-    recording_sid = form_data.get("RecordingSid")
     recording_url = form_data.get("RecordingUrl") + ".mp3"
 
-    print(f"Recording available for call {call_sid}: {recording_url}")
+    if call_data[call_sid] and call_data[call_sid]["receiver_speech"]:
+        call_result = " ".join(call_data[call_sid]["receiver_speech"])
+        call_status = analyze_response(call_result)
 
-    return {"status": "Recording received", "recording_url": recording_url}
+    else:
+        call_status = '4'
+
+    db = get_firestore_db()
+
+    event_docs = db.collection("events").where('id', '==', event_id).stream()
+    event_docs = [doc for doc in event_docs]
+
+    if not event_docs:
+        return {"status": "Event not found", "event_id": event_id}, 404
+
+    event_ref = event_docs[0].reference
+    event_data = event_docs[0].to_dict()
+
+    updated_calls = []
+    call_found = False
+    current_time = get_current_time()
+
+    if 'events' in event_data:
+        for call in event_data['events']:
+            if call.get('call_sid') == call_sid:
+                call['audio_url'] = recording_url
+                call['status'] = call_status
+                call['ended_at'] = current_time
+                call_found = True
+            updated_calls.append(call)
+
+    if not call_found:
+        return {"status": "Call record not found", "call_sid": call_sid}, 404
+
+    try:
+        event_ref.update({
+            'events': updated_calls,
+            'updated_at': current_time
+        })
+    except Exception as e:
+        print(f"Error save call record: {str(e)}")
+
+    print(f"Recording available for call {event_id}: {recording_url}")
+
+    return {"status": "Recording received and updated", "recording_url": recording_url}
 
 @router.post("/twilio-stream")
 async def twilio_stream(voice: str, company: str, purpose: str, requester: str, call_sid: Optional[str] = None):
@@ -145,8 +198,8 @@ async def twilio_stream(voice: str, company: str, purpose: str, requester: str, 
 
     if call_sid:
         count = call_data[call_sid]["count"]
-        if count < len(call_data[call_sid]["receiver_speech"]):
-            current_prompt = call_data[call_sid]["receiver_speech"][count]
+        if count <= len(call_data[call_sid]["receiver_speech"]):
+            current_prompt = call_data[call_sid]["receiver_speech"][count-1]
         else:
             current_prompt = ""
     else:
@@ -155,7 +208,7 @@ async def twilio_stream(voice: str, company: str, purpose: str, requester: str, 
     encoded_prompt = quote_plus(current_prompt)
 
     async with httpx.AsyncClient(timeout=30.0) as httpx_client:
-        res_url = f"https://aef0-78-46-38-10.ngrok-free.app/calls/say-prompt?voice={quote_plus(voice)}&company={quote_plus(company)}&purpose={quote_plus(purpose)}&requester={quote_plus(requester)}&prompt={encoded_prompt}"
+        res_url = f"https://beb1-78-46-38-10.ngrok-free.app/calls/say-prompt?voice={quote_plus(voice)}&company={quote_plus(company)}&purpose={quote_plus(purpose)}&requester={quote_plus(requester)}&prompt={encoded_prompt}"
         res = await httpx_client.get(res_url)
         if res.status_code != 200:
             raise HTTPException(status_code=res.status_code, detail="Failed to synthesize audio")
@@ -165,7 +218,8 @@ async def twilio_stream(voice: str, company: str, purpose: str, requester: str, 
             input="speech", 
             action=f"/calls/gather-complete?voice={quote_plus(voice)}&company={quote_plus(company)}&purpose={quote_plus(purpose)}&requester={quote_plus(requester)}", 
             method="POST", 
-            timeout=5, # Please optimize this timeout.
+            timeout=15, # Please optimize this timeout.
+            speech_timeout=3,
             language="ja-JP"
         )
         response.append(gather)
@@ -178,9 +232,14 @@ async def gather_complete(voice: str, company: str, purpose: str, requester: str
     form_data = await request.form()
     speech_result = form_data.get("SpeechResult")
     call_sid = form_data.get("CallSid")
-    print(f"Receiver said in Japanese: {speech_result}")
-    call_data[call_sid]["receiver_speech"].append(speech_result)
-    call_data[call_sid]["count"] += 1
+
+    if speech_result:
+        print(f"Receiver said in Japanese: {speech_result}")
+        call_data[call_sid]["receiver_speech"].append(speech_result)
+        call_data[call_sid]["count"] += 1
+    else:
+        print("No speech detected, continuing the call")
+
     response = VoiceResponse()
     response.redirect(f"/calls/twilio-stream?voice={quote_plus(voice)}&company={quote_plus(company)}&purpose={quote_plus(purpose)}&requester={quote_plus(requester)}&call_sid={call_sid}")
     
